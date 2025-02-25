b08c8d125c67a6f0e2b311d569c2aba1feb0f71b
ximenakaa <ximenaramirez46@gmail.com>
2025-02-25 00:43:58 -0300

    Agregar funcionalidad de "Amigo Secreto" con interfaz y estilos

---
+++ app2.js
@@ -0,0 +1,37 @@
+//agregar amigos
+        let listaAmigos = [];
+
+document.querySelector("#agregarAmigo").addEventListener("click", agregarAmigo);
+
+
+function agregarAmigo() {
+    // Obtener el valor del input
+    let nombreAmigo = document.querySelector("#amigo").value.trim();
+    
+    // Agregar el nombre a la lista usando el método push
+    if (nombreAmigo === '') {
+        alert('Ingresa un nombre');
+        return;
+    }
+    
+    listaAmigos.push(nombreAmigo);
+
+    // Crear un nuevo elemento <li> 
+    let lista = document.querySelector("#listaAmigos");
+    let nuevoAmigo = document.createElement("li");
+    nuevoAmigo.textContent = nombreAmigo;
+    lista.appendChild(nuevoAmigo);
+    // Limpiar el campo de entrada para el próximo nombre
+    document.querySelector("#amigo").value = "";
+}
+
+function sortearAmigo() {
+// sorteo
+
+    document.getElementById('listaAmigos').innerHTML = '';
+    let sortearAmigo = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSecreto = listaAmigos[sortearAmigo];
+    document.getElementById("resultado").innerHTML = `<span style="color: black;">El amigo secreto es:</span> ${amigoSecreto}`;
+}
+
+
